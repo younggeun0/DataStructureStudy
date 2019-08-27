dddd960135557c2f2af279fc82292b0f2e71716c
minj0i <h_hoh@naver.com>
2019-08-27 20:06:42 +0900

    1-1알고리즘이란_190827

---
+++ week1/minj0i/src/work1/Max1.java
@@ -0,0 +1,19 @@
+package work1;
+
+import java.util.Scanner;
+
+class Max1 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("세 정수의 최댓값을 구합니다.");
+		System.out.println("a의 값 : "); int a = stdIn.nextInt();
+		System.out.println("b의 값 : "); int b = stdIn.nextInt();
+		System.out.println("c의 값 : "); int c = stdIn.nextInt();
+		int max = a;
+		if (b > max) max = b;
+		if (c > max) max = c;
+		
+		System.out.println("최댓값은 " + max + "입니다.");
+	}
+}
