1d0be666806a04a0cfab82e822d522da1ec5cd71
minj0i <h_hoh@naver.com>
2019-09-03 14:22:39 +0900

    TIL_190903

---
+++ week2/minj0i/SeqSearchSen.java
@@ -0,0 +1,43 @@
+package chap03;
+
+import java.util.Scanner;
+
+public class SeqSearchSen {
+// 선형 검색 (보초법)
+	static int seqSearchSen(int[] a, int n, int key) {
+		int i = 0;
+		
+		a[n] = key;
+		
+		while(true) {
+			if (a[i] == key)
+				break;
+			i++;
+		}
+		return i == n ? -1 : i;
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("요솟수 : ");
+		int num = stdIn.nextInt();
+		int[] x = new int[num + 1];
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print("x["+i+"] : ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ");
+		int ky = stdIn.nextInt();
+		
+		int idx = seqSearchSen(x, num, ky);
+		
+		if(idx == -1)
+			System.out.println("그 값의 요소는 없습니다.");
+		else
+			System.out.println(ky+"은(는) + x["+idx+"]에 있습니다.");
+	}//main
+
+}//class
