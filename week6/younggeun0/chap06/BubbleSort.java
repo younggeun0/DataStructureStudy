1d8cbbbd21c7d8bbcdd6716b843de3bd86eb3518
Young Oh <34850791+younggeun0@users.noreply.github.com>
2019-09-08 15:42:06 +0900

    Create 정렬

---
+++ week6/younggeun0/chap06/BubbleSort.java
@@ -0,0 +1,48 @@
+package chap06;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+// 버블 정렬(버전 1)
+	
+	// a[idx1]과 a[idx2]의 값을 바꿈
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	// 버블 정렬
+	static void bubbleSort(int[] a, int n) {
+		for (int i=0; i<n-1; i++) {
+			for(int j=n-1; j>i; j--) {
+				if(a[j-1] > a[j])
+					swap(a, j-1, j);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("버블 정렬(버전1)");
+		System.out.print("요솟수 : ");
+		int n = sc.nextInt();
+		int[] x = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			System.out.print("x["+i+"] = ");
+			x[i] = sc.nextInt();
+		}
+		
+		bubbleSort(x, n);
+		
+		System.out.println("오름차순으로 정렬됨");
+		for(int i=0; i<n; i++) {
+			System.out.println("x["+i+"] = "+x[i]);
+		}
+		
+	}
+	
+
+}
